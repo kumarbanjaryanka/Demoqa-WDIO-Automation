21bb726ca3d23d7a0d3295bafbd8bfe9afbe3e95
ESOLUTIONS\Kumar.yanka <kumar.yanka@trivium-esolutions.com>
2025-06-26 15:02:45 +0530

    Configured WDIO

---
+++ test/specs/home.page.ts
@@ -0,0 +1,11 @@
+
+import LoginPage from '../pageobjects/login.page'
+import { expect } from '@wdio/globals'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+    
+       
+    })
+})
